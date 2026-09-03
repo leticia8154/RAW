bc6f6d89c4644468dbd2b31a71b3aa3d80589b76
Letícia Araújo <leticia.araujoa@sempreceub.com>
2026-09-03 13:59:53 +0000

    Atualização

--- src/config/spotify.js
+++ src/config/spotify.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-export const clientId = "SEU_CLIENT_ID_AQUI"; 
-export const redirectUri = "http://localhost:5173/"; 
+export const clientId = "490018501b90452fa8b5802b78b7ed63";
+export const redirectUri = "http://127.0.0.1:5173/";
 
 export const scopes = [
   "user-read-currently-playing",
